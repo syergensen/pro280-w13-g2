9b917e8aedc2847a7a0c88f3fcdb14b5525718d5
robbyzahm <zahmr597@gmail.com>
2013-02-27 18:33:46 -0700

    file added some a housing servlet and housing class in the model, this is my first commit for the project

---
+++ src/Model/Housing.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Robert
+ * Date: 2/20/13
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Housing {}
