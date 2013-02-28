19e374221cba7a396af553766ce176d616b6a832
robbyzahm <zahmr597@gmail.com>
2013-02-27 18:33:46 -0700

    file added some a housing servlet and housing class in the model, this is my first commit for the project

---
+++ src/Servlet/Housing.java
@@ -0,0 +1,27 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Robert
+ * Date: 2/20/13
+ * Time: 6:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Housing extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
+}
