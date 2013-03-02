3efd335b419aeaeffce8c9c4cf97104b4a2203f3
brdegenars <bradleydegenaars@gmail.com>
2013-03-01 19:15:45 -0700

    Created login servlet, edited post grad JSP, and edited web.xml to include new login servlet.

---
+++ src/Servlet/Login.java
@@ -0,0 +1,26 @@
+package Servlet;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brdegenaars
+ * Date: 3/1/13
+ * Time: 6:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Login extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        String userName = (String) request.getAttribute("userName");
+        String password = (String) request.getAttribute("password");
+
+        // TODO: so now I have the username and pass, I just need to do is forward the info the the proper url, and get the authorized username and role from NU
+
+        response.sendRedirect("URL TO NUID SERVICE");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
