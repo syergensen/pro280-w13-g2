d54d3353eae399e487955b493319028aaa4dd843
brdegenars <bradleydegenaars@gmail.com>
2013-03-08 19:33:16 -0700

    Created login filter to redirect to NUID login page hosted by NU. Created start servlet to accommodate NUID service redirection.

---
+++ src/Filters/LoginFilter.java
@@ -0,0 +1,36 @@
+package Filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brdegenaars
+ * Date: 3/8/13
+ * Time: 5:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        Principal userPrincipal = request.getUserPrincipal();
+
+        if (userPrincipal == null && !"/Start".equals(request.getContextPath())){
+
+            String fullRequestPath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+            ((HttpServletResponse) response).sendRedirect("http://my.neumont.edu/nuid/service.aspx?ReturnUrl=" + fullRequestPath + "/Start");
+        } else
+            chain.doFilter(req, response);
+    }
+
+    public void init(FilterConfig config) throws ServletException {}
+
+    public void destroy() {}
+}
---
+++ src/Servlet/StartServlet.java
@@ -0,0 +1,26 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brdegenaars
+ * Date: 3/8/13
+ * Time: 7:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@WebServlet("/Start")
+public class StartServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
